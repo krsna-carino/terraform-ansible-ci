5da485c87632c09204bb46b6ee54238f2064bb72
krsna-carino <guduribalakrishna7@gmail.com>
2025-11-04 23:55:31 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,11 @@
-output "frontend_public_ip" { value = aws_instance.frontend.public_ip }
-output "backend_public_ip"  { value = aws_instance.backend.public_ip }
-output "inventory_path"     { value = var.inventory_output_path }
+output "frontend_public_ip" {
+  value = aws_instance.frontend.public_ip
+}
+
+output "backend_public_ip" {
+  value = aws_instance.backend.public_ip
+}
+
+output "inventory_path" {
+  value = var.inventory_output_path
+}

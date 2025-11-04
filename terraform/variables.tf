c3bdbe40de86e19811f0170b79e02807a878aa61
krsna-carino <guduribalakrishna7@gmail.com>
2025-11-04 23:36:50 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "region" { type = string default = "us-east-1" }
+variable "key_name" { type = string }
+variable "ssh_cidr" { type = string default = "0.0.0.0/0" }
+
+variable "amazon_linux_ami_id" { type = string default = null }
+variable "ubuntu_ami_id" { type = string default = null }
+
+variable "inventory_output_path" {
+  type    = string
+  default = "${path.module}/../ansible/inventory.generated.yaml"
+}

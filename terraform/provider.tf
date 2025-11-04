570fa6eeaffa095a67b46c51055b2c74da480643
krsna-carino <guduribalakrishna7@gmail.com>
2025-11-04 23:36:20 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,1 +1,22 @@
+terraform {
+  required_version = ">= 1.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.4"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = ">= 2.2"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
 

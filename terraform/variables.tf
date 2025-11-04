c864e1cf1e6729f6ff9c7a4ba4f55b3c581de309
krsna-carino <guduribalakrishna7@gmail.com>
2025-11-04 23:54:32 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,26 @@
-variable "region" { type = string default = "us-east-1" }
-variable "key_name" { type = string }
-variable "ssh_cidr" { type = string default = "0.0.0.0/0" }
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
 
-variable "amazon_linux_ami_id" { type = string default = null }
-variable "ubuntu_ami_id" { type = string default = null }
+variable "key_name" {
+  type = string
+}
+
+variable "ssh_cidr" {
+  type    = string
+  default = "0.0.0.0/0"
+}
+
+variable "amazon_linux_ami_id" {
+  type    = string
+  default = null
+}
+
+variable "ubuntu_ami_id" {
+  type    = string
+  default = null
+}
 
 variable "inventory_output_path" {
   type    = string
